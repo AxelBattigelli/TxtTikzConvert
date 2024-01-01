740bd0de0631427e61fb65b28f0e620e405d054e
Axel Battigelli <146363061+AxelBattigelli@users.noreply.github.com>
2024-01-01 16:49:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,19 +6,6 @@
 ** Axel BATTIGELLI
 */
 
-/*
- * TO DO
- * 
- * tant que exit & loop
- * si -> fin si
- * 
- * bullet est la fin totale pas la fin d'une condition
- * fin si ou fin tantque ==> tmp
- * remplacer tmp par l'intruction suivante
- * reprendre le draw depuis un tmp
- * 
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -79,6 +66,7 @@ public class Main {
         StringBuilder dernierNoeud = new StringBuilder("(debut)");
         StringBuilder codeTikzgenerate = new StringBuilder();
         int t_value = 1;
+
         // Génération du logigramme en parcourant chaque ligne de l'algorithme
         for (int i = 1; i < lignes.length; i++) {
             // Analyse de la ligne pour déterminer la structure et générer le code Tikz correspondant
@@ -112,20 +100,83 @@ public class Main {
     private static String genererDrawLigne(StringBuilder codeTikzgenerate, StringBuilder auxTikzgenerate) {
         StringBuilder drawTikzgenerate = new StringBuilder();
         String[] lines = codeTikzgenerate.toString().split("\n");
-    
+        String[] lines2 = auxTikzgenerate.toString().split("\n");
+
         for (int i = 0; i < lines.length - 1; i++) {
             if (!lines[i].contains("\\textbullet") && !(lines[i + 1].contains("#finsi")) && !(lines[i + 1].contains("#finttq"))) {
-                drawTikzgenerate.append("\\draw[->] (t").append(i + 1).append(".south) to (t").append(i + 2).append(".north);\n");
+                drawTikzgenerate.append("\\draw [->] (t").append(i + 1).append(".south) to (t").append(i + 2).append(".north);\n");
             }
         }
-        // tant que : exit 
-        // \draw [->](t3.east)|-(aux1.center)node[pos=1.3,align=center]{non}|-(aux.center)|-(t6.east);
-        // tant que : loop
-        // \draw [->](t5.west)|-(aux3.center)|-(aux2.center)|-(t3.west);
+
+        int nbConditional = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if ((lines[i].contains("#finsi")) || (lines[i].contains("#finttq"))) {
+                nbConditional++;
+            }
+        }
+
+        int emplacement = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].contains("#finttq")) {
+                drawTikzgenerate.append("\\draw [->](t")
+                                .append(extractValueT(lines2[emplacement]))
+                                .append(".east)|-(aux")
+                                .append(extractValueAux(lines2[emplacement]))
+                                .append(".center)node[pos=1.3,align=center]{non}|-(aux")
+                                .append(extractValueAux(lines2[emplacement + nbConditional]))
+                                .append(".center)|-(t")
+                                .append(extractValueT(lines2[emplacement + nbConditional]))
+                                .append(".east);\n");
+                drawTikzgenerate.append("\\draw [->](t")
+                                .append(extractValueT(lines2[emplacement + 3 * nbConditional]))
+                                .append(".west)|-(aux")
+                                .append(extractValueAux(lines2[emplacement + 3 * nbConditional]))
+                                .append(".center)-(aux")
+                                .append(extractValueAux(lines2[emplacement + 1 + nbConditional]))
+                                .append(".center)|-(t")
+                                .append(extractValueT(lines2[emplacement + 1 + nbConditional]))
+                                .append(".west);\n");
+                emplacement++;
+            }
+            if (lines[i].contains("#finsi")) {
+                drawTikzgenerate.append("\\draw [->](t")
+                                .append(extractValueT(lines2[emplacement]))
+                                .append(".east)|-(aux")
+                                .append(extractValueAux(lines2[emplacement]))
+                                .append(".center)node[pos=1.3,align=center]{non}|-(aux")
+                                .append(extractValueAux(lines2[emplacement + nbConditional]))
+                                .append(".center)|-(t")
+                                .append(extractValueT(lines2[emplacement + nbConditional]))
+                                .append(".east);\n");
+                drawTikzgenerate.append("\\draw [->](t")
+                                .append(extractValueT(lines2[emplacement + 3 * nbConditional]))
+                                .append(".west)|-(aux")
+                                .append(extractValueAux(lines2[emplacement + 3 * nbConditional]))
+                                .append(".center)node[pos=1.3,align=center]{non}|-(aux")
+                                .append(extractValueAux(lines2[emplacement + 1 + nbConditional]))
+                                .append(".center)|-(t")
+                                .append(extractValueT(lines2[emplacement + 1 + nbConditional]))
+                                .append(".west);\n");
+                emplacement++;
+            }
+        }
         return drawTikzgenerate.toString();
     }
     
+    private static String extractValueT(String str) {
+        int firstIndexOfT = str.indexOf("t");
+        int secondIndexOfT = str.indexOf("t", firstIndexOfT + 1);
+        int indexOfClosingBracket = str.indexOf("]", secondIndexOfT);
     
+        return str.substring(secondIndexOfT + 1, indexOfClosingBracket);
+    }
+
+    private static String extractValueAux(String str) {
+        int indexOfT = str.indexOf("x");
+        int indexOfClosingBracket = str.indexOf(")", indexOfT);
+    
+        return str.substring(indexOfT + 1, indexOfClosingBracket);
+    }
 
     private static String genererAuxLigne(StringBuilder codeTikzgenerate) {
         StringBuilder auxTikzgenerate = new StringBuilder();
@@ -218,7 +269,6 @@ public class Main {
                 }
             }
         }
-
         return auxTikzgenerate.toString();
     }
 
